ce402293fc962e7470d59f573b0bae5541e84272
Linuxpizi <1360447275@qq.com>
2017-07-29 15:54:54 +0800

    init

--- test/node.js
+++
@@ -1,3 +0,0 @@
-var _ = require('lodash')
-console.log(_.min([1,2,3]))
-console.log(NaN === NaN)
--- vue-learn/src/demo/lesson1.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by Administrator on 2017/5/22.
- */
-
-var str = "hello javascript"
-console.log(this.str)
-// var str = "hello javascript"
